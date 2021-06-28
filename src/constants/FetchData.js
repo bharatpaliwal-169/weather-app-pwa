f16efd03f850b1bb3d1dcd0e2b11d7aaec15fad1
Bharat Paliwal <18uec169@lnmiit.ac.in>
2021-06-29 00:43:19 +0530

    .env is hack

--- src/constants/FetchData.js
+++ src/constants/FetchData.js
@@ -1,13 +1,14 @@
 import axios from 'axios'
 
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
+const API_KEY = '1330d1fb6e148db6c5a128f1cd8faedb'
 
 export const fetchWeather = async (query) => {
   const { data } = await axios.get(URL, {
       params: {
           q: query,
           units: 'metric',
-          APPID: process.env.REACT_APP_API_KEY,
+          APPID: API_KEY,
       }
   });
 
